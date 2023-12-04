c53fee7e938f2833dc5487660e8bdad34e6c9d54
MatteoLeonforte <mleonforte@student.ethz.ch>
2023-12-04 17:52:18 +0100

    Update Mujoco controller

--- teleop/scripts/low_level_controller/MuJoCo_controller/mujoco_controller.py
+++ teleop/scripts/low_level_controller/MuJoCo_controller/mujoco_controller.py
@@ -15,22 +15,26 @@ class GripperControllerMujocoSim:
     self.d = mujoco.MjData(self.m)
     self.simulation_thread = threading.Thread(target=self.run_viewer)
     self.lock = threading.Lock()
-    gc.start_simulation()
+    self.sim_running = False
+    self.start_simulation()
+
 
   def run_viewer(self):
 
     with mujoco.viewer.launch_passive(self.m, self.d) as viewer:
-      while viewer.is_running():
+      while viewer.is_running() and self.sim_running:
         with self.lock:
           mujoco.mj_step(self.m, self.d)
           viewer.sync()
           time.sleep(0.01)
+      viewer.close()
 
   def start_simulation(self):
+    self.sim_running = True
     self.simulation_thread.start()
 
   def stop_simulation(self):
-    self.viewer.close()
+    self.sim_running = False
     self.simulation_thread.join()
   
   def init_joints(self, calibrate=False):
