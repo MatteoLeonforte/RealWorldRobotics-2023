ee8b8ddf4b88f1ed26b3a9d6e73116277b1e4bba
MatteoLeonforte <mleonforte@student.ethz.ch>
2023-12-04 17:58:18 +0100

    Update Mujoco controller auto

--- teleop/scripts/low_level_controller/MuJoCo_controller/mujoco_controller_auto.py
+++ teleop/scripts/low_level_controller/MuJoCo_controller/mujoco_controller_auto.py
@@ -15,22 +15,26 @@ class GripperControllerMujocoSim:
     self.d = mujoco.MjData(self.m)
     self.simulation_thread = threading.Thread(target=self.run_viewer)
     self.lock = threading.Lock()
+    self.sim_running = False
+    self.start_simulation()
+
 
   def run_viewer(self):
 
     with mujoco.viewer.launch_passive(self.m, self.d) as viewer:
-      while viewer.is_running():
+      while viewer.is_running() and self.sim_running:
         with self.lock:
-          mujoco.mj_step(gc.m, gc.d)
+          mujoco.mj_step(self.m, self.d)
           viewer.sync()
-          #print("Running simulation")
           time.sleep(0.01)
+      viewer.close()
 
   def start_simulation(self):
+    self.sim_running = True
     self.simulation_thread.start()
 
   def stop_simulation(self):
-    self.viewer.close()
+    self.sim_running = False
     self.simulation_thread.join()
   
   def init_joints(self, calibrate=False):
@@ -38,36 +42,20 @@ class GripperControllerMujocoSim:
 
   def command_joint_angles(self, angles):
     with self.lock:
-      print(f"Received joint angles: {angles}")
       self.d.ctrl[:] = angles
 
-    pass
 
 
 
-if __name__ == "__main__":
+# Main function
+
+if __name__ == '__main__':
+
   gc = GripperControllerMujocoSim()
-  print("Initialized GripperControllerMujocoSim")
-  gc.start_simulation()
-  print("Started simulation")
-  
-  # Send a sequence of commands to the gripper
-  num_actuators = 11
-  steps = np.linspace(0,math.pi/2,100)
-  s = 0
-  
   while True:
-    # Send a command to the gripper to move all actuators from 0 to 90 degrees and back to 0 degrees with a 0.5 second pause in between
-    while s<100:
-      angles = [steps[s]]*num_actuators
-      s = s+1
-      gc.command_joint_angles(angles)
-      mujoco.mj_step(gc.m, gc.d)
-      time.sleep(0.1)
-      
-    while s>0:
-      angles = [steps[s]]*num_actuators
-      s = s-1
-      gc.command_joint_angles(angles)
-      mujoco.mj_step(gc.m, gc.d)
-      time.sleep(0.1)
+    angles = [math.pi/2]*11
+    gc.command_joint_angles(angles)
+    time.sleep(1)
+    angles = [0]*11
+    gc.command_joint_angles(angles)
+    time.sleep(1)
