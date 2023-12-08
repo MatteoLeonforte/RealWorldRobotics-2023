bb8d7a92c118ca853d77f0be6ff0b190d80d5ff6
MatteoLeonforte <mleonforte@student.ethz.ch>
2023-12-08 17:56:43 +0100

    Test file

---
+++ teleop/scripts/test.py
@@ -0,0 +1,8 @@
+def map_angle(angle, from_range: list, to_range: list):
+
+    assert angle >= from_range[0] and angle <= from_range[1], "Angle must be in from_range"
+            
+    mapped_angle = (angle - from_range[0]) * (to_range[1] - to_range[0]) / (from_range[1] - from_range[0]) + to_range[0]
+    return mapped_angle
+
+print(map_angle(0.5, [0, 3], [0, 3]))
